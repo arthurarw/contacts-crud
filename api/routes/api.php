2a07336b3938a91b90ace0abca0ef009bc91f30c
Arthur Warmling <arthurwarmling1@gmail.com>
2024-09-14 11:18:04 -0300

    feat: add endpoint to show api status;

--- api/routes/api.php
+++ api/routes/api.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return response()->json(['message' => 'API is running!!']);
+});
+
 Route::group(['prefix' => 'users'], function () {
     Route::get('/', [UserController::class, 'index']);
     Route::post('/', [UserController::class, 'store']);
